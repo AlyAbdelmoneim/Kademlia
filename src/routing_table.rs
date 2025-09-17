09f394f7563ae48d9641638eca53f7484454d0cd
Aly <aly.abdelmoneim@student.giu-uni.de>
2025-09-17 16:04:22 +0300

    refactoring find k closest nodes in routing_table.rs

--- src/routing_table.rs
+++ src/routing_table.rs
@@ -39,35 +39,68 @@ impl RoutingTable {
         bucket.add(new_node);
     }
 
-    pub fn find_k_nearest_nodes(&self, target_id: [u8; 20]) -> Vec<Contact> {
-        let nodes = &mut Vec::<Contact>::new();
-        let target_node_bucket = &self.buckets[self.find_bucket(target_id)];
-        nodes.extend(target_node_bucket.get_nodes());
-        return self.push_nodes_in_bucket(target_node_bucket.i - 1, target_node_bucket.i + 1, nodes);
-    }
+    //pub fn find_k_nearest_nodes(&self, target_id: [u8; 20]) -> Vec<Contact> {
+    //    let nodes = &mut Vec::<Contact>::new();
+    //    let target_node_bucket = &self.buckets[self.find_bucket(target_id)];
+    //    nodes.extend(target_node_bucket.get_nodes());
+    //    return self.push_nodes_in_bucket(
+    //        target_node_bucket.i - 1,
+    //        target_node_bucket.i + 1,
+    //        nodes,
+    //    );
+    //}
+    //
+    //fn push_nodes_in_bucket(
+    //    &self,
+    //    left_bucket_id: usize,
+    //    right_bucket_id: usize,
+    //    nodes: &mut Vec<Contact>,
+    //) -> Vec<Contact> {
+    //    //TODO: also, is -1 and +1 correct? where is the binary tree?
+    //    //TODO: enhance and optimize, lots of useless gets and unnnecassry if conditions and shit...
+    //    if (nodes.len() > K) {
+    //        return nodes.drain(0..K).collect();
+    //    }
+    //    let left_bucket = self.buckets.get(left_bucket_id);
+    //    let right_bucket = self.buckets.get(right_bucket_id);
+    //    if (left_bucket.is_none() && right_bucket.is_none()) {
+    //        return nodes.clone();
+    //    }
+    //    if let Some(bucket) = left_bucket {
+    //        nodes.extend(bucket.get_nodes());
+    //    }
+    //    if let Some(bucket) = right_bucket {
+    //        nodes.extend(bucket.get_nodes());
+    //    }
+    //    return self.push_nodes_in_bucket(left_bucket_id - 1, right_bucket_id + 1, nodes);
+    //}
 
-    fn push_nodes_in_bucket(
-        &self,
-        left_bucket_id: usize,
-        right_bucket_id: usize,
-        nodes: &mut Vec<Contact>,
-    ) -> Vec<Contact> {
-        //TODO: also, is -1 and +1 correct? where is the binary tree?
-        //TODO: enhance and optimize, lots of useless gets and unnnecassry if conditions and shit...
-        if (nodes.len() > K) {
-            return nodes.drain(0..K).collect();
+    pub fn find_k_nearest_nodes(&self, target_id: [u8; 20]) -> Vec<Contact> {
+        let mut nodes = Vec::new();
+        let i = self.find_bucket(target_id);
+        nodes.extend(self.buckets[i].get_nodes());
+
+        let mut left: isize = i as isize - 1;
+        let mut right: usize = i + 1;
+
+        while nodes.len() < K && (left >= 0 || right < ID_BITS) {
+            if left >= 0 {
+                nodes.extend(self.buckets[left as usize].get_nodes());
+                left -= 1;
+            }
+            if right < ID_BITS {
+                nodes.extend(self.buckets[right].get_nodes());
+                right += 1;
+            }
         }
-        let left_bucket = self.buckets.get(left_bucket_id);
-        let right_bucket = self.buckets.get(right_bucket_id);
-        if (left_bucket.is_none() && right_bucket.is_none()) {
-            return nodes.clone();
+
+        // sort the contacts by distance to target_id
+        nodes.sort_by_key(|contact| Distance::new(&contact.node_id, &target_id));
+
+        if nodes.len() > K {
+            nodes.truncate(K);
         }
-        if let Some(bucket) = left_bucket {
-            nodes.extend(bucket.get_nodes());
-        }
-        if let Some(bucket) = right_bucket {
-            nodes.extend(bucket.get_nodes());
-        }
-        return self.push_nodes_in_bucket(left_bucket_id - 1, right_bucket_id + 1, nodes);
+
+        nodes
     }
 }
